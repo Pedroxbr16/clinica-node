0692030ac8168744a06e934c6a20282d12cb2ad7
pedro <pjusto@cge.rj.gov.br>
2024-11-20 14:58:20 -0300

    dev

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -8,8 +8,10 @@ import './css/Sidebar.css';
 function Sidebar({ onLogout }) {
   const [showPatientsList, setShowPatientsList] = useState(false);
   const [showAppointmentsList, setShowAppointmentsList] = useState(false);
-  const [showAdminList, setShowAdminList] = useState(false); 
+  const [showAdminList, setShowAdminList] = useState(false);
   const navigate = useNavigate();
+
+  const userType = localStorage.getItem('userType'); // Obtém o tipo de usuário do localStorage
 
   const togglePatientsList = () => {
     setShowPatientsList(!showPatientsList);
@@ -30,40 +32,94 @@ function Sidebar({ onLogout }) {
 
   return (
     <div className="sidebar">
-      <h2><Link to="/" className="text-white text-decoration-none">Home</Link></h2>
+      <h2>
+        <Link to="/" className="text-white text-decoration-none">
+          Home
+        </Link>
+      </h2>
       <ul className="nav flex-column">
-        <li className="nav-item">
-          <a href="#pacientes" className="nav-link text-white" onClick={togglePatientsList}>
-            <FontAwesomeIcon icon={faUser} className="me-2" style={{ color: '#ffcc00' }} /> Pacientes
-          </a>
-          <ul className={`nested-list list-unstyled ${showPatientsList ? 'show' : ''}`}>
-            <li><Link to="/listagemPaciente" className="text-white nav-link">Listagem</Link></li>
-            <li><Link to="/cadastro" className="nav-link text-white">Cadastro</Link></li>
-            <li><Link to="/pedido-exames" className="nav-link text-white">Exames</Link></li>
-            <li><Link to="/Historico" className="nav-link text-white">Historicos</Link></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a href="#agendamentos" className="nav-link text-white" onClick={toggleAppointmentsList}>
-            <FontAwesomeIcon icon={faCalendarAlt} className="me-2" style={{ color: '#28a745' }} /> Agendamentos
-          </a>
-          <ul className={`nested-list list-unstyled ${showAppointmentsList ? 'show' : ''}`}>
-            <li><Link to="/agenda" className="nav-link text-white">Mostrar Agenda</Link></li>
-            <li><Link to="/create-event" className="nav-link text-white">Criar Agendamento</Link></li>
-            <li><Link to="/create-event" className="nav-link text-white">Pré-Agendamento</Link></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a href="#administracao" className="nav-link text-white" onClick={toggleAdminList}>
-            <FontAwesomeIcon icon={faCog} className="me-2" style={{ color: '#343a40' }} /> Administração
-          </a>
-          <ul className={`nested-list list-unstyled ${showAdminList ? 'show' : ''}`}>
-            <li><Link to="/config" className="nav-link text-white">Configurações</Link></li>
-          </ul>
-        </li>
+        {/* Menu de Pacientes */}
+        {(userType === 'adm' || userType === 'medico' || userType === 'atendente') && (
+          <li className="nav-item">
+            <a href="#pacientes" className="nav-link text-white" onClick={togglePatientsList}>
+              <FontAwesomeIcon icon={faUser} className="me-2" style={{ color: '#ffcc00' }} /> Pacientes
+            </a>
+            <ul className={`nested-list list-unstyled ${showPatientsList ? 'show' : ''}`}>
+              {(userType === 'adm' || userType === 'atendente') && (
+                <>
+                  <li>
+                    <Link to="/listagemPaciente" className="text-white nav-link">
+                      Listagem
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/cadastro" className="nav-link text-white">
+                      Cadastro
+                    </Link>
+                  </li>
+                </>
+              )}
+              {(userType === 'adm' || userType === 'medico') && (
+                <>
+                  <li>
+                    <Link to="/pedido-exames" className="nav-link text-white">
+                      Exames
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/historico" className="nav-link text-white">
+                      Históricos
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </li>
+        )}
+
+        {/* Menu de Agendamentos */}
+        {(userType === 'adm' || userType === 'medico' || userType === 'atendente') && (
+          <li className="nav-item">
+            <a href="#agendamentos" className="nav-link text-white" onClick={toggleAppointmentsList}>
+              <FontAwesomeIcon icon={faCalendarAlt} className="me-2" style={{ color: '#28a745' }} /> Agendamentos
+            </a>
+            <ul className={`nested-list list-unstyled ${showAppointmentsList ? 'show' : ''}`}>
+              <li>
+                <Link to="/agenda" className="nav-link text-white">
+                  Mostrar Agenda
+                </Link>
+              </li>
+              {(userType === 'adm' || userType === 'atendente') && (
+                <li>
+                  <Link to="/create-event" className="nav-link text-white">
+                    Criar Agendamento
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </li>
+        )}
+
+        {/* Menu de Administração */}
+        {userType === 'adm' && (
+          <li className="nav-item">
+            <a href="#administracao" className="nav-link text-white" onClick={toggleAdminList}>
+              <FontAwesomeIcon icon={faCog} className="me-2" style={{ color: '#343a40' }} /> Administração
+            </a>
+            <ul className={`nested-list list-unstyled ${showAdminList ? 'show' : ''}`}>
+              <li>
+                <Link to="/config" className="nav-link text-white">
+                  Configurações
+                </Link>
+              </li>
+            </ul>
+          </li>
+        )}
       </ul>
       <div className="sidebar-footer mt-auto">
-        <button onClick={handleLogoutClick} className="logout-button">Deslogar</button>
+        <button onClick={handleLogoutClick} className="logout-button">
+          Deslogar
+        </button>
       </div>
     </div>
   );
