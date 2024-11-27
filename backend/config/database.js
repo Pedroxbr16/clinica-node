71c9905881178a1ffd024a09a410d85b2bbd1036
pedro <pjusto@cge.rj.gov.br>
2024-11-27 17:43:31 -0300

    variaveis de ambiente

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Carrega as variáveis do .env
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'pjusto',
-  password: '1234',
-  database: 'clinica'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
--- backend/server/app.js
+++ backend/server/app.js
@@ -1,62 +1,57 @@
-  // app.js ou outro nome do seu arquivo principal
-  const express = require('express');
-  const path = require('path');
-  const pacienteRoutes = require('../routes/pacienteRoutes'); 
-  const medicoRoutes = require('../routes/medicoRoutes');
-  const atendenteRoutes = require('../routes/atendenteRoutes');
-  const authRoutes = require('../routes/authRoutes');
-  const consultaRoutes = require('../routes/consultaRoutes'); // Importando a rota de consulta
-  const tipoConsultaRoutes = require('../routes/tipoConsultaRoutes'); // Importe a nova rota
-  const historicoRoutes = require('../routes/historicoRoutes');
-  const Exames = require('../routes/ExameRoutes');
-  const users = require('../routes/mobile/usersRoutes');
-  const preAgendamentoRoutes = require('../routes/mobile/preAgendamentoRoutes');
-  const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+require('dotenv').config(); // Carrega as variáveis do .env
 
-  const app = express();
+const pacienteRoutes = require('../routes/pacienteRoutes'); 
+const medicoRoutes = require('../routes/medicoRoutes');
+const atendenteRoutes = require('../routes/atendenteRoutes');
+const authRoutes = require('../routes/authRoutes');
+const consultaRoutes = require('../routes/consultaRoutes');
+const tipoConsultaRoutes = require('../routes/tipoConsultaRoutes');
+const historicoRoutes = require('../routes/historicoRoutes');
+const Exames = require('../routes/ExameRoutes');
+const users = require('../routes/mobile/usersRoutes');
+const preAgendamentoRoutes = require('../routes/mobile/preAgendamentoRoutes');
 
-  // Middleware para parsear JSON
-  app.use(express.json());
-    
-  // Configuração do CORS 
-  app.use(cors({
-    origin:[ 'http://localhost:8081',
-      'http://localhost:8081',
-      'exp://192.168.1.3:8081',
-      'exp://192.168.27.163:8081',
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://192.168.1.8',
-      'exp://192.168.1.8:8081'
-    ],// Caso esteja usando Expo Go no dispositivo // URL do seu frontend
-    methods: ['GET', 'POST','PUT','DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+const app = express();
 
-  // Middleware para servir arquivos estáticos
-  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// Middleware para parsear JSON
+app.use(express.json());
 
-  // Rotas
-  app.use('/pacientes', pacienteRoutes);
-  app.use('/medicos', medicoRoutes);
-  app.use('/atendente', atendenteRoutes);
-  app.use('/api', authRoutes);
-  app.use('/consultas', consultaRoutes); // Usando a rota de consulta
-  app.use('/tipos_consulta', tipoConsultaRoutes); // Use a rota para tipos de consulta
-  app.use('/historico', historicoRoutes);
-  app.use('/exames', Exames);
-  // rotas do mobile
-  app.use('/user',  users);
-  app.use('/pre-agendamentos', preAgendamentoRoutes);
+// Configuração do CORS
+const corsOrigins = process.env.CORS_ORIGINS.split(','); // Divide as origens configuradas no .env
+app.use(cors({
+  origin: corsOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-  // Iniciar o servidor
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-  });
+// Middleware para servir arquivos estáticos
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-  app.use((req, res, next) => {
-    console.log(`[${req.method}] ${req.url}`);
-    next();
-  });
-  
+// Rotas
+app.use('/pacientes', pacienteRoutes);
+app.use('/medicos', medicoRoutes);
+app.use('/atendente', atendenteRoutes);
+app.use('/api', authRoutes);
+app.use('/consultas', consultaRoutes);
+app.use('/tipos_consulta', tipoConsultaRoutes);
+app.use('/historico', historicoRoutes);
+app.use('/exames', Exames);
+
+// rotas do mobile
+app.use('/user', users);
+app.use('/pre-agendamentos', preAgendamentoRoutes);
+
+// Iniciar o servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+// Logger básico de requisições
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
